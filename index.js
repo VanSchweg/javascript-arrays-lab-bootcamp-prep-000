62e3c1890dfd9e517a27663dcf752d5190313309
Vanessa Schwegman <vschwegman@gmail.com>
2017-10-09 01:40:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const app = "I don't do much."
 
 var kittens = ['Milo', 'Otis', 'Garfield'];
 
-var function = destructivelyAppendKitten(name) {
+function destructivelyAppendKitten(name) {
   kittens.push(name);
 }
