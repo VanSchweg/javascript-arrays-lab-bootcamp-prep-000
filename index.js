103f96ba6518584bdccad0401e2251d62f3b5091
Vanessa Schwegman <vschwegman@gmail.com>
2017-10-09 02:34:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,6 +30,6 @@ function removeLastKitten() {
   return kittens.slice(0,2);
 }
 
-funtion removeFirstKitten() {
-  return kittens.slice(1,kittens.length);
+function removeFirstKitten() {
+  return kittens.slice(1);
 }
